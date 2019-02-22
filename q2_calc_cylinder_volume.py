c0d81d9fca5580055633e332caa3cf9349abf07e
Kennethkcpdhs <47784720+Kennethkcpdhs@users.noreply.github.com>
2019-02-22 22:06:26 +0800

    Create q2_calc_cylinder_volume.py

---
+++ q2_calc_cylinder_volume.py
@@ -0,0 +1,5 @@
+from math import pi
+radius=eval(input("What is the radius in m?:"))
+length=eval(input("What is the length in m?:"))
+volume=(radius**2*pi*length)
+print("the volume is(cm3): ",(volume))
