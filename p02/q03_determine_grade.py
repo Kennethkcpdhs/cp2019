25e2ba584d3bce0803c9850238f02b27d5e3cb45
Kennethkcpdhs <47784720+Kennethkcpdhs@users.noreply.github.com>
2019-02-26 21:34:43 +0800

    Create q03_determine_grade.py

---
+++ p02/q03_determine_grade.py
@@ -0,0 +1,17 @@
+score = eval(input("Enter score: "))
+if 0<=score<=34:
+    print("U")
+elif 35<=score<=44:
+    print("S")
+elif 45<=score<=49:
+    print("E")
+elif 50<=score<=54:
+    print("D")
+elif 55<=score<=59:
+    print("C")
+elif 60<=score<=69:
+    print("B")
+elif 70<=score<=100:
+    print("S")
+else:
+    print("Invalid! Score must be within 0-100.")
