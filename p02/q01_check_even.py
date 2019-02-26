bc3c93c2a5584f7674d40ac9eaffc25002d13c2c
Kennethkcpdhs <47784720+Kennethkcpdhs@users.noreply.github.com>
2019-02-26 21:11:31 +0800

    Create q01_check_even.py

---
+++ p02/q01_check_even.py
@@ -0,0 +1,9 @@
+def read_even():
+    number = eval(input("Enter number: "))
+    if number%2==0:
+        print(number,"is even")
+        
+    else:
+        print(number,"is odd")
+        
+read_even()
