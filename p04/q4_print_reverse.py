6f915f61be235c01793bd8c0ea0e57e830bab2c4
Kennethkcpdhs <47784720+Kennethkcpdhs@users.noreply.github.com>
2019-03-05 11:03:20 +0800

    Create q4_print_reverse.py

---
+++ p04/q4_print_reverse.py
@@ -0,0 +1,11 @@
+def reverse_int():
+    n = list((input("Enter a word: ")))
+    c = []
+    for k in n[::-1]:
+        u = ""
+        c.append(k)
+        
+    print (u.join(c))
+
+
+reverse_int()
